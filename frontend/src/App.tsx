a6231ce6439bb15dbb1450e73701cc9fcafa4e80
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-08-01 12:56:51 -0400

    added TODOs for future implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,4 +10,9 @@ function App() {
     );
 }
 
+// TODO: create mini forms for each image when they're uploaded, allowing the user to choose whether or not they want the images dithered
+// useUploadedFiles will need a function to handle the input from these forms, find which object in the imageState needs adjusting, and subsequently save the dithered file (or not) depending on preference in another key
+// TODO: create custom input component that consists of image name, image preview, and dropdown / radio to choose whether should be dithered or not
+// as an input changes, some handler function will find which object to adjust as a result (should i create nanoid's for each uploaded image? i think so just to make this easier)
+// the submit button for this form will run ditherHandler
 export default App;
