862606ab470b7365a2c66e705c908d34df2f1275
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-08-06 11:07:39 -0400

    initialized ImageInput

---
+++ frontend/src/components/ImageInput.tsx
@@ -0,0 +1,24 @@
+import { selectHandlerType, UploadedImage } from "../hooks/useUploadedFiles";
+
+interface ImageInputType {
+    id: string;
+    formId: string;
+    image: UploadedImage;
+    onChange: selectHandlerType;
+}
+
+export default function ImageInput({ id, formId, image, onChange }: ImageInputType) {
+    return (
+        <div className="flex flex-col items-center w-full" id={`${image.file.name}-${id}`}>
+            <h3 className="font-bold">{image.file.name}</h3>
+
+            <img className="h-full p-4" src={image.src} alt={image.file.name} />
+
+            <label htmlFor={id}>want to dither this image?</label>
+            <select value="N" onChange={(e) => onChange(id, e.target.value === "Y")} className="" form={formId} id={id}>
+                <option value="Y">dither it!</option>
+                <option value="N">don't dither it.</option>
+            </select>
+        </div>
+    );
+}
