f75ff09453026a9a5ca0a5dbeb6e41ccb3039b20
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-08-06 11:24:29 -0400

    fixed rendering of components in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,20 +7,34 @@ function App() {
     const [imgState, uploadHandler, selectHandler, ditherHandler] = useUploadedFiles([]);
 
     return (
-        <main className="flex flex-col items-center w-full h-screen gap-4 p-8">
+        <main className="flex flex-col items-center w-full h-[150vh] gap-4 p-8">
             <h1 className="font-bold">basic-dithering-app</h1>
             <FileUpload onUpload={uploadHandler} />
-            <form className="flex flex-row justify-between w-full gap-4 mb-16 h-2/5" id="dither-form">
-                {imgState.map((image) => (
-                    <ImageInput
-                        key={image.id}
-                        id={image.id}
-                        formId="dither-form"
-                        image={image}
-                        onChange={selectHandler}
-                    />
-                ))}
-            </form>
+            {imgState.length > 0 && (
+                <form className="flex flex-col items-center gap-12 h-2/5" id="dither-form">
+                    <div className="flex flex-row justify-between w-full gap-4 mb-16">
+                        {imgState.map((image) => (
+                            <ImageInput
+                                key={image.id}
+                                id={image.id}
+                                formId="dither-form"
+                                image={image}
+                                onChange={selectHandler}
+                            />
+                        ))}
+                    </div>
+                    <button
+                        formAction="submit"
+                        className="w-full h-16 text-white bg-black rounded-lg"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            imgState.map((img) => console.log(img.dither));
+                        }}
+                    >
+                        SUBMIT
+                    </button>
+                </form>
+            )}
         </main>
     );
 }
