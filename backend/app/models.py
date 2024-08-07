06b331a02eca2dd8d31f23ced9455d961ef20e38
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-08-07 18:05:32 -0400

    initialized necessary models and routes

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,8 +1,32 @@
 class UploadedImage:
-    def __init__(self, ) -> None:
-        self.id = id,
-        self.file = file
-        self.src = 
-        self.dither = 
-        self.ditheredImage = 
+    def __init__(self, image_id, file_name, src, dither, dithered_image) -> None:
+        self.id : str = image_id
+        self.file_name : str = file_name
+        self.src : str = src
+        self.dither : bool = dither
+        self.dithered_image = dithered_image # TODO: add type for this ?? idk ??
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'file_name': self.file_name,
+            'src': self.src,
+            'dither': self.dither,
+            'dithered_image': self.dithered_image,
+        }
     
+class UploadedImageList:
+    def __init__(self, images : list[UploadedImage]) -> None:
+        self.images = images
+    
+    def push(self, image : UploadedImage):
+        self.images.append(image)
+        return self.images
+
+    def to_dict_list(self):
+        image_list = []
+
+        for image in self.images:
+            image_list.append(image.to_dict())
+
+        return image_list
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,25 @@
 from flask import Blueprint, jsonify, request
+from .models import UploadedImage, UploadedImageList
 
 main = Blueprint('main', __name__)
 
 @main.route("/", methods=['POST'])
 def dither_images():
     data = request.get_json()
+
+    if not data:
+        return jsonify({'error': 'Invalid upload'}), 400
+    
+    images = UploadedImageList([])
+
+    for image in data:
+        uploadedImage = UploadedImage(
+            id=image.id,
+            file_name=image.file_name,
+            src=image.src,
+            dither=image.dither,
+            dithered_image=image.dithered_image
+        )
+        images.append(uploadedImage)
+
+    return jsonify(images.to_dict_list()), 201
