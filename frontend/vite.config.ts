96e4a660ed4d102757d4f416d75fd1f0fa74e9bf
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-08-08 15:18:55 -0400

    added cors proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:4001',
+        changeOrigin: true,
+        secure: false, // Set to false if you're not using HTTPS
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix if your Flask route is not prefixed
+      }
+    }
+  }
 })
