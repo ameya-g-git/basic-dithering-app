2231f617045c4577aaaa68d2e2312aa0510e61ff
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-08-08 15:50:52 -0400

    installed axios for http requests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,39 @@ import FileUpload from "./components/FileUpload";
 import useUploadedFiles from "./hooks/useUploadedFiles";
 import "./output.css";
 import ImageInput from "./components/ImageInput";
+import axios from "axios";
 
 function App() {
-    const [imgState, uploadHandler, selectHandler, ditherHandler] = useUploadedFiles([]);
+    const [imgState, uploadHandler, selectHandler] = useUploadedFiles([]);
+
+    async function submitImages() {
+        const formData = new FormData();
+
+        formData.append("images", JSON.stringify(imgState));
+
+        console.log(imgState);
+        try {
+            const response = await axios.post("/api", formData);
+
+            if (response.status === 200 || response.status === 201) {
+                console.log("Uploaded images to server");
+            } else {
+                console.error("Error:", response.statusText);
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+
+    // useEffect(() => {
+
+    //     }
+    // }, [imgState]);
+    // Used with backend to upload files to server
+    // for (const image of ditherState) {
+    //     console.log(image.dither);
+    // }
+    // return state;
 
     return (
         <main className="flex flex-col items-center w-full gap-4 p-8">
@@ -24,11 +54,11 @@ function App() {
                         ))}
                     </div>
                     <button
-                        formAction="submit"
+                        type="button"
                         className="w-full h-12 text-white bg-black rounded-lg"
                         onClick={(e) => {
                             e.preventDefault();
-                            imgState.map((img) => console.log(img.dither));
+                            submitImages();
                         }}
                     >
                         SUBMIT
